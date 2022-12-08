4c11142a28ca7e18277d688a769f6ea02ad7441e
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-08 12:19:14 -0800

    E

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: rfrefrf
+ *
+ * Return: efef
+ **/
+
+int main(void)
+{
+	char k[];
+
+	k = "\"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\"";
+	fprintf(stderr, "%s", k);
+	return (1);
+}
