9af7d5fe938579eb30e082d9d04f3ba9ee201544
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-08 10:38:39 -0800

    e

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main: wow
+ *
+ * Description: hey
+ *
+ * return : 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
