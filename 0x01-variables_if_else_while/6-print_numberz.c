b5f369e7bc97385604050caa9e3c875e08d781ec
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-09 13:07:24 -0800

    E

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'I am lazy'
+ *
+ * Return: zero
+ **/
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	while (i <= 9)
+	{
+		putchar(48 + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
