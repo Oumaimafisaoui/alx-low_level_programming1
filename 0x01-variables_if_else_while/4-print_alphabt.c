c0d71ad04c82ef2c812436cf2c320c5d0fd0db36
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-09 12:56:27 -0800

    E

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'I am lazy'
+ *
+ * Return: zero
+ **/
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		if(i == 'e' || i == 'q')
+			i++;
+		else
+			putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
