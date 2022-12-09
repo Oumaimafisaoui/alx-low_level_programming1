0355984f43cf2678dde415aefef59f092141e3fa
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-09 12:45:29 -0800

    push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'I am lazy'
+ *
+ * Return: zero
+ **/
+int main(void)
+{
+	char i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
