a4ed3b2f98b76837e871956454e2147fc4b21bd6
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-13 11:40:19 -0800

    e

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'print_alphabet is a function'
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for(i = 0; i < 10 ; i++)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+	}
+}
