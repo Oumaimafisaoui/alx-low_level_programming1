2f79bef296e81879cbc65085e3e15c2ef0b9b1b8
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-13 12:08:43 -0800

    e

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _isalpha - Something
+ *
+ * @c: ehkwbfehw
+ *
+ * Description: 'print_alphabet is a function'
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}	
