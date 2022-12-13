8706e8bf407d2aac1b4b8aa5276c087384e99b1a
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-13 14:28:12 -0800

    E

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * prin_to_98 - Something
+ *
+ * @n: j
+ *
+ * Description: 'print_alphabet is a function'
+ *
+ * Return: Always 0.
+ */
+
+int print_to_98(int n)
+{
+	if(n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ",n);
+		}
+	}
+	else if (n < 98)
+	{
+		for(; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d, ", n);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,5 @@ int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);
 #endif
