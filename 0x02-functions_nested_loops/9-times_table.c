a1ec9e26485b982a89fd91c0b2651cd31236a17a
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-13 14:00:04 -0800

    E

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * times_table - Something
+ *
+ * Description: 'print_alphabet is a function'
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int a, b, o;
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			o = a + b;
+			_putchar(44);
+			_putchar(32);
+			if(o <= 9)
+			{
+				_putchar(32);
+				_putchar(o + 48);
+			}
+			else
+			{
+				_putchar((o / 10) + 48);
+				_putchatr((o % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+
+}
