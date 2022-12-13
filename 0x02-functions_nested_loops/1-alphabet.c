f6e178fca4f982018f39e383417f94abd8eb97bd
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-13 11:23:40 -0800

    e

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -8,8 +8,8 @@
  * Return: ero
  **/
 
-int main(void)
+void print_alphabet(void)
 {
-	printf("_putchar\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
 	return (0);
 }
