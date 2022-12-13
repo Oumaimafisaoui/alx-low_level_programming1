3b035289b346ca296958ea527b2d141a04e31e95
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-13 11:43:50 -0800

    e

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: 'print_alphabet is a function'
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if(c >= 'a' && c <='z')
+		return(1);
+	return (0);
+}
