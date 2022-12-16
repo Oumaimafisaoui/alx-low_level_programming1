30e9e3dbe27c735546103675231f39ad166f7535
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-15 17:32:06 -0800

    E

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - tests function that prints
- * Return :0
+ * Return : 0
  */
 
 int main(void)
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+	
 	return (0);
 }
