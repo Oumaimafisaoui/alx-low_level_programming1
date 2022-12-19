8564ceb5567181158c3c4c476fe4a8f9813d6e26
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-19 13:38:10 -0800

    E

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * puts2 - takes a pointer and change its content
+ * @str: the param
+ * Description: rfrefr wedfweffew
+ * Return: zero
+ **/
+
+void print_array(int *a, int n)
+{
+	int i;
+	
+	i = 0;
+	for (;, i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ void _puts(char *str);
 void print_rev(char *str);
 void rev_string(char *s);
 void puts2(char *str);
+void print_array(int *a, int n);
 #endif
