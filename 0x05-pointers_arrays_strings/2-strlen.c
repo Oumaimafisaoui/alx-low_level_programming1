04933745340227b1887fb37ce98576dda3dc8777
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-19 12:14:41 -0800

    E

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _strlen - takes a pointer and change its content
+ * @s: the param
+ * @b: ecfeffe
+ * Description: rfrefr wedfweffew
+ * Return: zero
+ **/
+
+int _strlen(char *s)
+{
+	while(*s)
+	{
+		write(1, &*s, 1);
+		s++
+	}
+}
+
