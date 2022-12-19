e03a897555bb912c31e02627000639a34c4306bc
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-19 11:50:19 -0800

    E

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98: 'efefef'
- *
- * Description: rfrefrf
- *
+ * reset_to_98 - takes a pointer and change its content
+ * @n: the param
+ * Description: rfrefr wedfweffew
  * Return: zero
- *
- * */
+ **/
 
 void reset_to_98(int *n)
 {
 	int *a = n;
 	*a = 98;
 }
-
