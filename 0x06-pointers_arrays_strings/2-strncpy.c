3615ab4e7e65e2de52d3faf80008ecf7756681de
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-22 12:28:40 -0800

    e

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - efefwef
+ * @dest: efef
+ * @src: fwefew
+ * @n: wefefwfw
+ * Description: efeewf
+ * Return: wefefwe
+ **/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
