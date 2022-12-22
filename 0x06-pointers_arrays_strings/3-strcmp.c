6219db01dd387f34647b831abba5070fb45da017
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-22 12:34:25 -0800

    e

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - efwefwfw
+ * @s1: frfr
+ * @s2: ewfewf
+ * Description: efrf
+ * Return : efewf
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j= 0;
+	
+	while(j == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i))
+				break;
+		j = *(s1 + i) - *(s2 + i);
+		i++;
+
+	}
+	return (j);
+}
