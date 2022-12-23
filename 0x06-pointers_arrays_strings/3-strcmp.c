da1e9efb8e9c59388e180203c1453c511c23fa2c
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-23 14:12:33 -0800

    E

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,12 +16,14 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 	j = 0;
-	while i(j == 0)
+	while (j == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-				break;
+		{
+			break;
+		}
 		j = *(s1 + i) - *(s2 + i);
-		i++;
+		j++;
 	}
 	return (j);
 }
